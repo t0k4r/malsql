75eb02136f2b3d1ba4c4dbbbb90202c66f2c01d9
t0k4r <mi03to08@gmail.com>
2023-06-27 00:20:04 +0200

    new new schema

--- scrap/anime/schema.sql
+++ scrap/anime/schema.sql
@@ -5,11 +5,11 @@ CREATE TABLE IF NOT EXISTS public.seasons (
 	CONSTRAINT seasons_pk PRIMARY KEY (id),
 	CONSTRAINT seasons_un UNIQUE (name_of)
 );
-CREATE TABLE IF NOT EXISTS public.types_of (
+CREATE TABLE IF NOT EXISTS public.anime_types (
 	id serial NOT NULL,
 	name_of varchar NOT NULL,
-	CONSTRAINT types_of_pk PRIMARY KEY (id),
-	CONSTRAINT types_of_un UNIQUE (name_of)
+	CONSTRAINT anime_types_pk PRIMARY KEY (id),
+	CONSTRAINT anime_types_un UNIQUE (name_of)
 );
 CREATE TABLE IF NOT EXISTS public.animes (
 	id serial NOT NULL,
@@ -23,24 +23,10 @@ CREATE TABLE IF NOT EXISTS public.animes (
 	aired_to date NULL,
 	season_id int NULL,
 	type_of_id int NULL,
-	CONSTRAINT newtable_pk PRIMARY KEY (id),
-	CONSTRAINT newtable_un UNIQUE (mal_url),
+	CONSTRAINT animes_pk PRIMARY KEY (id),
+	CONSTRAINT animes_un UNIQUE (mal_url),
 	CONSTRAINT animes_fk FOREIGN KEY (season_id) REFERENCES public.seasons(id),
-	CONSTRAINT animes_fk_1 FOREIGN KEY (type_of_id) REFERENCES public.types_of(id)
-);
-CREATE TABLE IF NOT EXISTS public.info_types (
-	id serial NOT NULL,
-	name_of varchar NOT NULL,
-	CONSTRAINT infos_pk PRIMARY KEY (id),
-	CONSTRAINT infos_un UNIQUE (name_of)
-);
-CREATE TABLE IF NOT EXISTS public.anime_infos (
-	anime_id int NOT NULL,
-	info_type_id int NOT NULL,
-	value varchar NOT NULL,
-	CONSTRAINT anime_infos_un UNIQUE (anime_id, info_type_id, value),
-	CONSTRAINT anime_infos_fk FOREIGN KEY (anime_id) REFERENCES public.animes(id),
-	CONSTRAINT anime_infos_fk_1 FOREIGN KEY (info_type_id) REFERENCES public.info_types(id)
+	CONSTRAINT animes_fk_1 FOREIGN KEY (type_of_id) REFERENCES public.anime_types(id)
 );
 CREATE TABLE IF NOT EXISTS public.episodes (
 	id serial NOT NULL,
@@ -51,4 +37,25 @@ CREATE TABLE IF NOT EXISTS public.episodes (
 	CONSTRAINT episodes_pk PRIMARY KEY (id),
 	CONSTRAINT episodes_un UNIQUE (anime_id,index_of),
 	CONSTRAINT episodes_fk FOREIGN KEY (anime_id) REFERENCES public.animes(id)
+);
+CREATE TABLE IF NOT EXISTS public.info_types (
+	id serial NOT NULL,
+	name_of varchar NOT NULL,
+	CONSTRAINT info_types_pk PRIMARY KEY (id),
+	CONSTRAINT info_types_un UNIQUE (name_of)
+);
+CREATE TABLE IF NOT EXISTS public.infos (
+	id serial NOT NULL,
+	value_of varchar NOT NULL,
+	type_id int NOT NULL,
+	CONSTRAINT infos_pk PRIMARY KEY (id),
+	CONSTRAINT infos_un UNIQUE (value_of),
+	CONSTRAINT infos_fk FOREIGN KEY (type_id) REFERENCES public.info_types(id)
+);
+CREATE TABLE IF NOT EXISTS public.anime_infos (
+	anime_id int4 NOT NULL,
+	info_id int4 NOT NULL,
+	CONSTRAINT anime_infos_un UNIQUE (anime_id, info_id),
+	CONSTRAINT anime_infos_fk FOREIGN KEY (anime_id) REFERENCES public.animes(id),
+	CONSTRAINT anime_infos_fk_1 FOREIGN KEY (info_id) REFERENCES public.infos(id)
 );
