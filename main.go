c52c3c5ab0b612234642dff1398eb732efd1cb9b
t0k4r <mi03to08@gmail.com>
2024-03-12 00:35:05 +0100

    burn it all

--- main.go
+++ main.go
@@ -1,32 +1,9 @@
 package main
 
 import (
-	"MalSql/scrap"
-	"flag"
-	"log"
-	"os"
-
-	_ "github.com/lib/pq"
+	"fmt"
 )
 
 func main() {
-	defer func() {
-		if err := recover(); err != nil {
-			log.Println("panic occurred:", err)
-		}
-	}()
-
-	var opts scrap.Options
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	flag.IntVar(&opts.Start, "start", 1, "start index")
-	flag.IntVar(&opts.End, "end", 75000, "end index")
-	flag.BoolVar(&opts.File, "file", false, "dump to sql file not database")
-	flag.BoolVar(&opts.Skip, "skip", false, "skip done animes (not availible if -file)")
-	flag.BoolVar(&opts.Update, "update", false, "on conflict update")
-	flag.StringVar(&opts.Driver, "driver", "sqlite3", "postgres or sqlite3")
-	flag.StringVar(&opts.Conn, "conn", "./MalSql.sqlite", "database connection string")
-	flag.BoolVar(&opts.Env, "env", false, "read database connection from env (MALSQL_DB)")
-	flag.Parse()
-
-	scrap.New(opts).Run()
+	fmt.Println("hello, world")
 }
